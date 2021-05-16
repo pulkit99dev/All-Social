a657149c4cf4cc0481436e0d5e6096b1948d62e9
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-05-16 20:07:48 +0530

    typos

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -24,7 +24,7 @@ module.exports.home = async function(req, res){
         let users = await User.find({});
 
         return res.render('home', {
-            title: "Codeial | Home",
+            title: "All-Social | Home",
             posts:  posts,
             all_users: users
         });
